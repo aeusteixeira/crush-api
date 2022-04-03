dfcc9566a30871983ad6510b322f54cd51087ff3
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2022-04-03 12:06:09 -0300

    Ajustado titulo da página inicial

--- pages/index.js
+++ pages/index.js
@@ -3,25 +3,9 @@ import { useState } from 'react';
 function Home(){
     return (
         <div>
-            <h1>Home</h1>
-            <Contador />
-        </div>
-    )
-}
-
-function Contador(){
-    const [count, setCount] = useState(0);
-
-    function increment(){
-        setCount(count + 1);
-    }
-
-    return (
-        <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
-            </button>
+            <h1>
+                Página inicial
+            </h1>
         </div>
     )
 }
