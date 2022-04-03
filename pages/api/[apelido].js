ab548fd940d21ebec98b0f392b649ba32f7eafaf
Matheus Teixeira <contato.matheusteixeira@gmail.com>
2022-04-03 14:11:08 -0300

    Ajustado apelido como id principal

--- pages/api/[apelido].js
+++ pages/api/[apelido].js
@@ -1,6 +1,9 @@
-function crush(request, response) {
+async function crush(request, response) {
+    const endpoint = process.env.ENDPOINT_CRUSH;
     const apelido = request.query.apelido;
-    
+    const crush = await fetch(`https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=${token}&team_name=${apelido}`);
+    const json = await crush.json();
+
 }
 
 export default crush;
